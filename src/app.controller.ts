5a68c4ec77090c1967fd55173ff17139431ee815
pzzyf <2279948211@qq.com>
2025-05-08 10:11:27 +0800

    base

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Put } from "@nestjs/common";
 import { AppService } from "./app.service";
 
-@Controller()
+@Controller("app")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("list")
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("list")
+  create(): string {
+    return "123";
+  }
+
+  // @Get("user_*")
+  // getUser() {
+  //   return "getUser";
+  // }
+
+  @Put("list/:id")
+  update() {
+    return "update";
+  }
+
+  @Put("list/user")
+  updateUser() {
+    return { userId: 1 };
   }
 }
