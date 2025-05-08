d3d2b309171e15b956945caff322db27288ad293
pzzyf <2279948211@qq.com>
2025-05-08 13:54:57 +0800

    拦截器

---
+++ src/posts/posts.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("posts")
+export class PostsEntity {
+  @PrimaryGeneratedColumn()
+  id: number; // 标记为主列，值自动生成
+
+  // 标记为主列，值自动生成
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ length: 20 })
+  author: string;
+
+  @Column("text")
+  content: string;
+
+  @Column({ default: "" })
+  thumb_url: string;
+
+  @Column("tinyint")
+  type: number;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  create_time: Date;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  update_time: Date;
+}
